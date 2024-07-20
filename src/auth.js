b58cdcdec2bb2ca5c4abfed64e7882825874326e
Danil <kabirdanilov14@gmail.com>
2024-07-20 15:35:48 +0500

    extending session with custom fields

--- src/auth.js
+++ src/auth.js
@@ -3,8 +3,33 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 
 import { prisma } from "@/server/db";
 import authConfig from "@/auth.config";
+import { getUserById } from "@/server/user/getUser";
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      console.log({ token, session });
+
+      // добавляем в сессию нужные поля
+      if (token.sub && session.user) session.user.id = token.sub;
+      if (token.role && session.user) session.user.role = token.role;
+      if (token.username && session.user) session.user.username = token.role;
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+      token.username = existingUser.username;
+
+      return token;
+    },
+  },
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
   ...authConfig,
